d1fe316526de67ffa107926b02f2bf80dc23a52b
Francisco Rossi <rossifrancisco12@gmail.com>
2017-02-10 10:38:10 -0300

    test

--- resources/views/nuevolibro.blade.php
+++ resources/views/nuevolibro.blade.php
@@ -12,7 +12,7 @@
                 <span class="input-group-addon" id="basic-addon1"></span>
                 <input name="autor" type="text" class="form-control" placeholder="Autor" aria-describedby="basic-addon1">
             </div>
-            <hr>
+            <hr>    
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1"></span>
                 <input name="precio" type="text" class="form-control" placeholder="Precio" aria-describedby="basic-addon1">
